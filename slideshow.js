1ea713589377a4dc414e108e2925c97b3d68ca8b
gabriellsux <gabrielpereiramartins49@gmail.com>
2024-08-09 22:43:15 -0300

    Finalizando o projeto

--- slideshow.js
+++ slideshow.js
@@ -1,23 +1,27 @@
 'use strict';
 
 const images = [
-    {'id': '1', 'url': './img/chrono.jpg'},
-    {'id': '2', 'url': './img/inuyasha.jpg'},
-    {'id': '3', 'url': './img/tenchi.jpg'},
-    {'id': '4', 'url': './img/tenjhotenge.jpg'},
-    {'id': '5', 'url': './img/yuyuhakusho.jpg'},
-    {'id': '6', 'url': './img/ippo.png'},
+    { 'id': '1', 'url':'./img/chrono.jpg' },
+    { 'id': '2', 'url':'./img/inuyasha.jpg' },
+    { 'id': '3', 'url':'./img/tenchi.jpg' },
+    { 'id': '4', 'url':'./img/tenjhotenge.jpg' },
+    { 'id': '5', 'url':'./img/yuyuhakusho.jpg' },
+    { 'id': '6', 'url':'./img/ippo.png' },
 ]
 
 const containerItems = document.querySelector('#container-items');
 
-const loadImages = (images, container) => {
-    images.forEach (images => {
-        container.innerHTML += `<div><img src='${image.url}'></div>`
+const loadImages = ( images, container ) => {
+    images.forEach ( image => {
+        container.innerHTML += `
+            <div class='item'>
+                <img src='${image.url}'
+            </div>
+        `
     })
 }
 
-loadImages(images, containerItems);
+loadImages( images, containerItems );
 
 let items = document.querySelectorAll('.item');
 
@@ -25,3 +29,12 @@ const previous = () => {
     containerItems.appendChild(items[0]);
     items = document.querySelectorAll('.item');
 }
+
+const next = () => {
+    const lastItem = items[items.length - 1];
+    containerItems.insertBefore( lastItem, items[0] );
+    items = document.querySelectorAll('.item');
+}
+
+document.querySelector('#previous').addEventListener('click', previous);
+document.querySelector('#next').addEventListener('click', next);
